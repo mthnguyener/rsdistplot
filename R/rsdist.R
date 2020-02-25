a67b6ee3530ed47fd661c1033e6df23111d3935b
mthnguyener <minhtuan.h.nguyen@gmail.com>
2020-02-25 11:54:15 -0500

    package complete

--- R/rsdist.R
+++ R/rsdist.R
@@ -1,36 +1,36 @@
 #' Random Sample Distribution
 #'
 #' Allow users to generate random samples from 4 different distributions: Normal("n"), t("t"), F("f"), Binomial("b")
-#' Users should only have to enter the name of the distribution ("n", "t", "f", "b") and the number of samples (n) to get a sample with the default argument values.
-#' Users should be able to specify arguments for the parameters of each distribution to override the defaults.
+#' Users only need to enter the name of the distribution ("n", "t", "f", "b") and the number of samples (n) to get a sample with the default argument values.
+#' Users can specify arguments for the parameters of each distribution to override the defaults.
 #' Normal: Mean (x) and Standard Deviation (y)
 #' t: Degrees of Freedom (x)
 #' F: Numerator (x) and Denominator Degrees of Freedom (y)
 #' Binomial: Size-Number of Trials (x),and p-Probability of Success on each Trial (y)
-#' Users should be able to choose to set the seed or not and if they set the seed the value of the seed (the last arguments or s).
+#' Users can choose to set the seed or not and if they set the seed the value of the seed. To set seed, add s = interger (ex. s = 1) when calling the function.
 #'
 #' @param d Character variable representing 4 types of distributions (n = Normal, t = t, f = F, and b = Binomial)
 #' @param n Interger variable for sample size
-#' @param x Parameter 1 of the distribution
-#' @param y Parameter 2 of the distribution
-#' @param s Set seed to s interger
+#' @param x Parameter 1 of the distribution (default = 50)
+#' @param y Parameter 2 of the distribution (default = 50)
+#' @param s Set seed to s interger (default = NULL)
 #'
-#' @return
-#' @export
+#' @return Random variates having normal, t, f, or binomial distribution
+#' @export rsdist
 #' @importFrom stats rnorm rt rf rbinom
 #'
-#' @examples rsdist("n", n = 50, x = 6, y = 5, s = 1)
-rsdist <- function(d, n, x, y=NULL, s=NULL) {
+#' @examples rsdist("n", n = 1000, x = 6, y = 5, s = 1)
+rsdist <- function(d, n, x = 50, y = 50, s=NULL) {
   set.seed(s)
   if (d == "n") {
     rnorm(n = n, mean = x, sd = y)
   } else if (d == "t") {
-    set.seed(y)
+    y = NULL
     rt(n = n, df = x)
   } else if (d == "f") {
     rf(n = n, df1 = x, df2 = y)
   }  else if (d == "b") {
-    rbinom(n = n, size = x, prob = y)
+    rbinom(n = n, size = x, prob = y/100)
   } else {
     stop(paste0("distribution is not normal (\"n\"), t (\"t\"), F (\"f\"), Binomial = (\"b\")"))
   }
--- R/rshist.R
+++ R/rshist.R
@@ -1,50 +1,67 @@
 #' Random Sample Histogram
 #'
 #' Allow users to generate plots of histograms for random samples from 4 different distributions: Normal("n"), t("t"), F("f"), Binomial("b")
-#' Users should only have to enter the name of the distribution ("n", "t", "f", "b") and the number of samples (n) to get a result
-#' Users should be able to include additional parameters for each distribution as well as the number of bins for the histogram
+#' Users only need to enter the name of the distribution ("n", "t", "f", "b") and the number of samples (n) to get a result
+#' Users can include additional parameters for each distribution as well as the number of bins for the histogram
 #' Normal: Mean (x) and Standard Deviation (y)
 #' t: Degrees of Freedom (x)
 #' F: Numerator (x) and Denominator Degrees of Freedom (y)
 #' Binomial: Size-Number of Trials (x),and p-Probability of Success on each Trial (y)
-#' Users should be able to choose to set the seed or not and if they set the seed the value of the seed (the last arguments or s).
+#' Users can choose to set the seed or not and if they set the seed the value of the seed. To set seed, add s = interger (ex. s = 1) when calling the function.
 #'
 #' @param d Character variable representing 4 types of distributions (n = Normal, t = t, f = F, and b = Binomial)
 #' @param n Interger variable for sample size
-#' @param x Parameter 1 of the distribution
-#' @param y Parameter 2 of the distribution
-#' @param s Set seed to s interger
+#' @param x Parameter 1 of the distribution (default = 50)
+#' @param y Parameter 2 of the distribution (default = 50)
+#' @param s Set seed to s interger (default = NULL)
+#' @param b Set binwidth (default = 2)
 #'
-#' @return
-#' @export
+#' @return A plot of random variates having normal, t, f, or binomial distribution
+#' @export rshist
 #' @importFrom ggplot2 ggplot aes geom_histogram coord_flip ggtitle xlab
 #' @importFrom stats rnorm rt rf rbinom
 #' @importFrom ggthemes theme_economist scale_color_economist
 #'
-#' @examples rsdist("n", n = 50, x = 6, y = 5, s = 1)
-rshist <- function(d, n, x, y=NULL, s=NULL, ...) {
+#' @examples rshist("n", n = 1000, x = 6, y = 5, s = 1)
+rshist <- function(d, n, x = 50, y = 50, s=NULL, b=2) {
   set.seed(s)
   if (d == "n") {
     normal <- rnorm(n = n, mean = x, sd = y)
     normal <- as.data.frame(normal)
-    ggplot(normal, aes(x=normal)) + geom_histogram(color="darkblue", fill="lightblue", ...) + theme_economist() + scale_color_economist() +
-      ggtitle("Random Normal Distribution Histogram") + xlab("Results")
+    ggplot(normal, aes(x=normal)) +
+      geom_histogram(color="darkblue", fill="lightblue", binwidth = b) +
+      theme_economist() +
+      scale_color_economist() +
+      ggtitle("Random Normal Distribution Histogram") +
+      xlab(paste(n, "Samples in", b, "Bins"))
   } else if (d == "t") {
-    set.seed(y)
+    y = NULL
     tdist <- rt(n = n, df = x)
     tdist <- as.data.frame(tdist)
-    ggplot(tdist, aes(x=tdist)) + geom_histogram(color="darkblue", fill="lightblue", ...) + theme_economist() + scale_color_economist() +
-      ggtitle("Random T Distribution Histogram") + xlab("Results")
+    ggplot(tdist, aes(x=tdist)) +
+      geom_histogram(color="darkblue", fill="lightblue", binwidth = b) +
+      theme_economist() +
+      scale_color_economist() +
+      ggtitle("Random T Distribution Histogram") +
+      xlab(paste(n, "Samples in", b, "Bins"))
   } else if (d == "f") {
     fdist <- rf(n = n, df1 = x, df2 = y)
     fdist <- as.data.frame(fdist)
-    ggplot(fdist, aes(x=fdist)) + geom_histogram(color="darkblue", fill="lightblue", ...) + theme_economist() + scale_color_economist() +
-      ggtitle("Random F Distribution Histogram") + xlab("Results")
+    ggplot(fdist, aes(x=fdist)) +
+      geom_histogram(color="darkblue", fill="lightblue", binwidth = b) +
+      theme_economist() +
+      scale_color_economist() +
+      ggtitle("Random F Distribution Histogram") +
+      xlab(paste(n, "Samples in", b, "Bins"))
   }  else if (d == "b") {
-    binom <- rbinom(n = n, size = x, prob = y)
+    binom <- rbinom(n = n, size = x, prob = y/100)
     binom <- as.data.frame(binom)
-    ggplot(binom, aes(x=binom)) + geom_histogram(color="darkblue", fill="lightblue", ...) + theme_economist() + scale_color_economist() +
-      ggtitle("Random Binomial Distribution Histogram") + xlab("Results")
+    ggplot(binom, aes(x=binom)) +
+      geom_histogram(color="darkblue", fill="lightblue", binwidth = b) +
+      theme_economist() +
+      scale_color_economist() +
+      ggtitle("Random Binomial Distribution Histogram") +
+      xlab(paste(n, "Samples in", b, "Bins"))
   } else {
     stop(paste0("distribution is not normal (\"n\"), t (\"t\"), F (\"f\"), Binomial = (\"b\")"))
   }
